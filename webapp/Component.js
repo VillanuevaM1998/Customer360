9137056eb2af7eda387984ce08195051a92d13f3
vlnmDelaware <melvin.villanueva@delaware.pro>
2021-02-18 12:46:34 +0000

    commit push from trial subbAccount

--- webapp/Component.js
+++ webapp/Component.js
@@ -20,11 +20,11 @@ sap.ui.define([
 			// call the base component's init function
 			UIComponent.prototype.init.apply(this, arguments);
 
+		// set the device model
+			this.setModel(models.createDeviceModel(), "device");
+		
 			// enable routing
 			this.getRouter().initialize();
-
-			// set the device model
-			this.setModel(models.createDeviceModel(), "device");
 		}
 	});
 });
--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,5 +1,5 @@
 sap.ui.define([
-	"com/delaware/vlnm/trac201919/controller/BaseController"
+	"com/delaware/vlnm/trac2019/controller/BaseController"
 ], function (BaseController) {
 	"use strict";
 
